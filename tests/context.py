5987c383b081d017513e8eda9336446c5bc03dbd
JoschD <joschua.dilly@gmail.com>
2019-08-01 19:15:08 +0200

    Fixed Context

--- tests/context.py
+++ tests/context.py
@@ -1,6 +1,6 @@
 import sys
 from os.path import abspath, join, dirname, pardir
-root_path = abspath(join(dirname(__file__), pardir, pardir))
+root_path = abspath(join(dirname(__file__), pardir))
 if root_path not in sys.path:
     sys.path.insert(0, root_path)
 tfs_path = join(root_path, "sdds")
