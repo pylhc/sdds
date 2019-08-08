3db18fa230f03a4143b6a062be63ea12582ba298
JoschD <joschua.dilly@gmail.com>
2019-08-08 17:54:41 +0200

    Bump version: 0.1.1 → 0.1.2

--- sdds/__init__.py
+++ sdds/__init__.py
@@ -1,7 +1,7 @@
 """Exposes SddsFile, read_sdds and write_sdds directly in sdds namespace."""
 from sdds.writer import write_sdds
 from sdds.reader import read_sdds
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 from sdds.classes import SddsFile
 # aliases
 read = read_sdds
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="sdds",
-    version="0.1.1",
+    version="0.1.2",
     description="Read and write sdds files.",
     long_description=README,
     long_description_content_type="text/markdown",
