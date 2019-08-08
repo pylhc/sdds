7b02cf4bce082572ca60a8b71f8e02f1a9ed8a2f
JoschD <joschua.dilly@gmail.com>
2019-08-08 17:23:34 +0200

    #10 Fixed all imports

--- tests/context.py
+++ tests/context.py
@@ -3,8 +3,4 @@ from os.path import abspath, join, dirname, pardir
 root_path = abspath(join(dirname(__file__), pardir))
 if root_path not in sys.path:
     sys.path.insert(0, root_path)
-sdds_path = join(root_path, "sdds")
-if sdds_path not in sys.path:
-    sys.path.insert(0, sdds_path)
-
 import sdds
