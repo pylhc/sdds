ff9fb84bd8f12f4b877ea93e3c38477495952fa6
Maël Le Garrec <mael@legarrec.org>
2019-10-18 11:52:01 +0200

    Update CI config (#15)
    Fixes #14

--- setup.py
+++ setup.py
@@ -1,16 +1,53 @@
+import os
 import pathlib
-from setuptools import setup
+import re
+from setuptools import setup, find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
+# Name of the module
+MODULE_NAME = 'sdds'
+
+# Dependencies for the module itself
+DEPENDENCIES = ['numpy>=1.14.1',
+                ]
+
+# Test dependencies that should only be installed for test purposes
+TEST_DEPENDENCIES = ['pytest>=5.2',
+                     'pytest-cov>=2.6',
+                     'hypothesis>=3.23.0',
+                     'attrs>=19.2.0'
+                     ]
+
+# pytest-runner to be able to run pytest via setuptools
+SETUP_REQUIRES = ['pytest-runner']
+
+# Extra dependencies for tools
+EXTRA_DEPENDENCIES = {'doc': ['sphinx',
+                              'travis-sphinx',
+                              'sphinx_rtd_theme']
+                      }
+
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+
+def get_version():
+    """ Reads package version number from package's __init__.py. """
+    with open(os.path.join(
+        os.path.dirname(__file__), MODULE_NAME, '__init__.py'
+    )) as init:
+        for line in init.readlines():
+            res = re.match(r'^__version__ = [\'"](.*)[\'"]$', line)
+            if res:
+                return res.group(1)
+
+
 # This call to setup() does all the work
 setup(
-    name="sdds",
-    version="0.1.3",
+    name='sdds',
+    version=get_version(),
     description="Read and write sdds files.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -22,7 +59,12 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    packages=["sdds"],
-    install_requires=["numpy"],
+    packages=find_packages(include=(MODULE_NAME,)),
+    install_requires=DEPENDENCIES,
+    tests_require=DEPENDENCIES + TEST_DEPENDENCIES,
+    extras_require=EXTRA_DEPENDENCIES,
+    setup_requires=SETUP_REQUIRES
 )
--- tests/context.py
+++
@@ -1,6 +0,0 @@
-import sys
-from os.path import abspath, join, dirname, pardir
-root_path = abspath(join(dirname(__file__), pardir))
-if root_path not in sys.path:
-    sys.path.insert(0, root_path)
-import sdds
