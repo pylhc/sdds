e6392bde7a824296f3dfebd998573d3eed86ab49
Felix Soubelet <felix.soubelet@protonmail.com>
2020-10-07 18:35:23 +0200

    refactor #21 refactor setup.py

--- setup.py
+++ setup.py
@@ -1,70 +1,75 @@
-import os
 import pathlib
-import re
-from setuptools import setup, find_packages
 
+import setuptools
+
+MODULE_NAME = "sdds"
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# Name of the module
-MODULE_NAME = 'sdds'
-
-# Dependencies for the module itself
-DEPENDENCIES = ['numpy>=1.14.1',
-                ]
-
-# Test dependencies that should only be installed for test purposes
-TEST_DEPENDENCIES = ['pytest>=5.2',
-                     'pytest-cov>=2.6',
-                     'hypothesis>=3.23.0',
-                     'attrs>=19.2.0'
-                     ]
-
-# pytest-runner to be able to run pytest via setuptools
-SETUP_REQUIRES = ['pytest-runner']
-
-# Extra dependencies for tools
-EXTRA_DEPENDENCIES = {'doc': ['sphinx',
-                              'travis-sphinx',
-                              'sphinx_rtd_theme']
-                      }
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
+TOPLEVEL_DIR = pathlib.Path(__file__).parent.absolute()
+ABOUT_FILE = TOPLEVEL_DIR / MODULE_NAME / "__init__.py"
+README = TOPLEVEL_DIR / "README.md"
 
 
-def get_version():
-    """ Reads package version number from package's __init__.py. """
-    with open(os.path.join(
-        os.path.dirname(__file__), MODULE_NAME, '__init__.py'
-    )) as init:
-        for line in init.readlines():
-            res = re.match(r'^__version__ = [\'"](.*)[\'"]$', line)
-            if res:
-                return res.group(1)
+def about_package(init_posixpath: pathlib.Path) -> dict:
+    """
+    Return package information defined with dunders in __init__.py as a dictionary, when
+    provided with a PosixPath to the __init__.py file.
+    """
+    about_text: str = init_posixpath.read_text()
+    return {
+        entry.split(" = ")[0]: entry.split(" = ")[1].strip('"')
+        for entry in about_text.strip().split("\n")
+        if entry.startswith("__")
+    }
 
 
-# This call to setup() does all the work
-setup(
-    name='sdds',
-    version=get_version(),
-    description="Read and write sdds files.",
-    long_description=README,
+ABOUT_SDDS = about_package(ABOUT_FILE)
+
+with README.open("r") as docs:
+    long_description = docs.read()
+
+# Dependencies for the package itself
+DEPENDENCIES = [
+    "numpy>=1.19.0",
+]
+
+# Extra dependencies
+EXTRA_DEPENDENCIES = {
+    "test": ["pytest>=5.2", "pytest-cov>=2.6", "hypothesis>=3.23.0", "attrs>=19.2.0"],
+    "doc": [
+        "sphinx",
+        "travis-sphinx",
+        "sphinx_rtd_theme",
+    ],  # TODO: remove travis sphinx after GA
+}
+EXTRA_DEPENDENCIES.update(
+    {"all": [elem for list_ in EXTRA_DEPENDENCIES.values() for elem in list_]}
+)
+
+
+setuptools.setup(
+    name=ABOUT_SDDS["__title__"],
+    version=ABOUT_SDDS["__version__"],
+    description=ABOUT_SDDS["__description__"],
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pylhc/sdds",
-    author="pyLHC",
-    author_email="pylhc@github.com",
+    author=ABOUT_SDDS["__author__"],
+    author_email=ABOUT_SDDS["__author_email__"],
+    url=ABOUT_SDDS["__url__"],
     license="MIT",
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     packages=find_packages(include=(MODULE_NAME,)),
     install_requires=DEPENDENCIES,
-    tests_require=DEPENDENCIES + TEST_DEPENDENCIES,
+    tests_require=EXTRA_DEPENDENCIES["test"],
     extras_require=EXTRA_DEPENDENCIES,
-    setup_requires=SETUP_REQUIRES
 )
