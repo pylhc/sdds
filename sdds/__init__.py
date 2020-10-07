00bfc122c84ce97a7811656d3a1ea6b8a9f8a05b
Felix Soubelet <felix.soubelet@protonmail.com>
2020-10-07 18:34:19 +0200

    refactor #21 refactor __init__.py

--- sdds/__init__.py
+++ sdds/__init__.py
@@ -1,8 +1,19 @@
 """Exposes SddsFile, read_sdds and write_sdds directly in sdds namespace."""
 from sdds.writer import write_sdds
 from sdds.reader import read_sdds
-__version__ = "0.1.3"
 from sdds.classes import SddsFile
+
+__title__ = "sdds"
+__description__ = "SDDS file handling."
+__url__ = "https://github.com/pylhc/sdds"
+__version__ = "0.1.3"
+__author__ = "pylhc"
+__author_email__ = "pylhc@github.com"
+__license__ = "MIT"
+
 # aliases
 read = read_sdds
 write = write_sdds
+
+
+__all__ = [read, SddsFile, write, __version__]
