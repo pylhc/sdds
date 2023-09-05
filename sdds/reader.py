421323965d92cade452d38983ce8be52354a7f9a
Felix Soubelet <felix.soubelet@protonmail.com>
2023-09-05 14:53:21 +0200

    absolutely no subscripting on 3.8

--- sdds/reader.py
+++ sdds/reader.py
@@ -24,12 +24,12 @@ from sdds.classes import (ENCODING, NUMTYPES_CAST, NUMTYPES_SIZES, Array,
 
 # ----- Providing Opener Abstractions for the Reader ----- #
 
-# On Python 3.8, we cannot subscript contextlib.AbstractContextManager, which became possible 
-# with PEP 585 in Python 3.9. We will check for the runtime version and simply not subscript
-# the AbstractContextManager if running on 3.8. The cost here is degraded typing.
+# On Python 3.8, we cannot subscript contextlib.AbstractContextManager or collections.abc.Callable,
+# which became possible with PEP 585 in Python 3.9. We will check for the runtime version and simply
+# not subscript if running on 3.8. The cost here is degraded typing.
 # TODO: remove this conditional once Python 3.8 has reached EoL and we drop support for it
 if sys.version_info < (3, 9, 0):  # we're running on 3.8, which is our lowest supported
-    OpenerType = Callable[[os.PathLike], AbstractContextManager]
+    OpenerType = Callable
 else:
     OpenerType = Callable[[os.PathLike], AbstractContextManager[io.BufferedIOBase]]
 
